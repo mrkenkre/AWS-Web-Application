4d2299444fd2cedb01bf2de3ea3f3c7dcd1ffb03
mrkenkre <mrkenkre@gmail.com>
2023-10-13 16:13:25 -0400

    Updated test 4

--- test/healthztest.js
+++ test/healthztest.js
@@ -2,25 +2,32 @@ const chai = require("chai");
 const chaiHttp = require("chai-http");
 const app = require("../app"); // Import your Express app here
 const expect = chai.expect;
-const sequelize = require("../config/sequelize");
+// const sequelize = require("../config/sequelize");
 
 chai.use(chaiHttp);
 
-describe("Check database connection", () => {
-  it("should return status 200 and success message", (done) => {
-    chai
-      .request(app)
-      .get("/healthz2") // Change this to the correct route path
-      .end((err, res) => {
-        if (err) {
-          console.log("Connection NOT successful: " + err);
-          return done(err);
-          // process.exit(1);
-        } else {
-          expect(res).to.have.status(200);
-          console.log("Connection successful.");
-          done();
-        }
-      });
+// describe("Check database connection", () => {
+//   it("should return status 200 and success message", (done) => {
+//     chai
+//       .request(app)
+//       .get("/healthz2") // Change this to the correct route path
+//       .end((err, res) => {
+//         if (err) {
+//           console.log("Connection NOT successful: " + err);
+//           return done(err);
+//           // process.exit(1);
+//         } else {
+//           expect(res).to.have.status(200);
+//           console.log("Connection successful.");
+//           done();
+//         }
+//       });
+//   });
+// });
+
+describe("Healthz Endpoint", () => {
+  it("should return a 200 status", async () => {
+    const response = await chai.request(app).get("/healthzz");
+    expect(response.status).to.equal(200);
   });
 });
