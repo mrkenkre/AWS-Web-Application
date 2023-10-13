862cfff823d68d805709b620100266dce8a0b8df
mrkenkre <mrkenkre@gmail.com>
2023-10-13 01:17:13 -0400

    Updated test

--- test/healthztest.js
+++ test/healthztest.js
@@ -6,16 +6,21 @@ const sequelize = require("../config/sequelize");
 
 chai.use(chaiHttp);
 
-describe("/Check database connection", () => {
-  it("should return status 200 and success message", async (done) => {
-    try {
-      await sequelize.authenticate();
-      done();
-      console.log("Connection successful.");
-      process.exit(0);
-    } catch (error) {
-      console.log("Connection NOT successful." + error);
-      process.exit(1);
-    }
+describe("Check database connection", () => {
+  it("should return status 200 and success message", (done) => {
+    chai
+      .request(app)
+      .get("/healthz2") // Change this to the correct route path
+      .end((err, res) => {
+        if (err) {
+          console.log("Connection NOT successful: " + err);
+          return done(err);
+          // process.exit(1);
+        } else {
+          expect(res).to.have.status(200);
+          console.log("Connection successful.");
+          done();
+        }
+      });
   });
 });
