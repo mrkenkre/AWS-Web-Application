2181fdc5e638361d92c7971d0eaac8df5050f588
mrkenkre <mrkenkre@gmail.com>
2023-10-12 22:00:44 -0400

    Updated test

--- test/healthztest.js
+++ test/healthztest.js
@@ -2,23 +2,20 @@ const chai = require("chai");
 const chaiHttp = require("chai-http");
 const app = require("../app"); // Import your Express app here
 const expect = chai.expect;
+const sequelize = require("../config/sequelize");
 
 chai.use(chaiHttp);
 
-describe("/healthz API", () => {
-  it("should return status 200 and success message", (done) => {
-    chai
-      .request(app)
-      .get("/healthz")
-      .end((err, res) => {
-        if (err) {
-          console.error(err);
-          process.exit(1);
-        } else {
-          expect(res).to.have.status(200);
-          done();
-          process.exit(0);
-        }
-      });
+describe("/Check database connection", () => {
+  it("should return status 200 and success message", async (done) => {
+    try {
+      await sequelize.authenticate();
+      done();
+      console.log("Connection successful.");
+      process.exit(0);
+    } catch (error) {
+      console.log("Connection NOT successful." + error);
+      process.exit(1);
+    }
   });
 });
