bd81464482416802ac840dff2fb8945f132f24a4
mrkenkre <44857610+mrkenkre@users.noreply.github.com>
2023-10-25 01:32:30 -0400

    Updated workflows (#51)

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,21 +3,21 @@
 sleep 20
 
 sudo apt-get update
-sudo apt install -y nodejs npm mariadb-server zip unzip
+sudo apt install -y nodejs npm unzip
+
+sudo groupadd csye6225
+sudo useradd -s /bin/false -g csye6225 -d /opt/csye6225 -m csye6225
+sudo cp csye6225.service /etc/systemd/system
+systemctl daemon-reload
+sudo systemctl enable csye6225
+sudo systemctl start csye6225
+sudo systemctl restart csye6225
+sudo systemctl stop csye6225
 
 cd /tmp
-sudo mv /tmp/webapp.zip /opt/webapp.zip
-cd /opt
+sudo mv /tmp/webapp.zip /opt/csye6225/webapp.zip
+cd /opt/csye6225
 sudo unzip webapp.zip
-sudo mv /tmp/.env /opt/.env
+sudo chown -R csye6225:csye6225 /opt/csye6225
+
 sudo npm install
-
-sudo systemctl start mariadb
-sudo systemctl enable mariadb
-
-sudo mysql -u root <<EOF
-CREATE USER '$DB_USER'@'localhost' IDENTIFIED BY '$DB_PASS';
-CREATE DATABASE $DB_NAME;
-GRANT ALL PRIVILEGES ON *.* TO $DB_USER@'localhost' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
-EOF
