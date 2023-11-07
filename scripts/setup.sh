f7b40ee1da739b4e3efb3623907675725188c1a3
mrkenkre <mrkenkre@gmail.com>
2023-11-07 04:38:29 -0500

    Modified setup script.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sleep 20
+sleep 10
 
 sudo apt-get update
 sudo apt install -y nodejs npm unzip
@@ -21,45 +21,46 @@ sudo touch /opt/aws/amazon-cloudwatch-agent/cloudwatch-config.json
 sudo chmod 644 /opt/aws/amazon-cloudwatch-agent/cloudwatch-config.json
 
 cd /opt/aws/amazon-cloudwatch-agent/
-sudo sh -c 'echo "{
-  "agent": {
-      "metrics_collection_interval": 10,
-      "logfile": "/var/logs/amazon-cloudwatch-agent.log"
+sudo sh -c "echo '{
+  \"agent\": {
+      \"metrics_collection_interval\": 10,
+      \"logfile\": \"/var/logs/amazon-cloudwatch-agent.log\"
   },
-  "logs": {
-      "logs_collected": {
-          "files": {
-              "collect_list": [
+  \"logs\": {
+      \"logs_collected\": {
+          \"files\": {
+              \"collect_list\": [
                   {
-                      "file_path": "/var/log/csye6225_stdop.log",
-                      "log_group_name": "csye6225",
-                      "log_stream_name": "webapp"
+                      \"file_path\": \"/var/log/csye6225_stdop.log\",
+                      \"log_group_name\": \"csye6225\",
+                      \"log_stream_name\": \"webapp\"
                   },
                    {
-                      "file_path": "/var/log/csye6225_error.log",
-                      "log_group_name": "csye6225",
-                      "log_stream_name": "webapp"
+                      \"file_path\": \"/var/log/csye6225_error.log\",
+                      \"log_group_name\": \"csye6225\",
+                      \"log_stream_name\": \"webapp\"
                   },
                    {
-                      "file_path": "/var/log/auth.log",
-                      "log_group_name": "ec2-security",
-                      "log_stream_name": "audit-log"
+                      \"file_path\": \"/var/log/auth.log\",
+                      \"log_group_name\": \"ec2-security\",
+                      \"log_stream_name\": \"audit-log\"
                   }
               ]
           }
       },
-      "log_stream_name": "cloudwatch_log_stream"
+      \"log_stream_name\": \"cloudwatch_log_stream\"
   },
-  "metrics": {
-    "metrics_collected": {
-       "statsd": {
-          "service_address": "8125",
-          "metrics_collection_interval": 15,
-          "metrics_aggregation_interval": 300
+  \"metrics\": {
+    \"metrics_collected\": {
+       \"statsd\": {
+          \"service_address\": \"8125\",
+          \"metrics_collection_interval\": 15,
+          \"metrics_aggregation_interval\": 300
        }
     }
   }
-}" > cloudwatch-config.json'
+}' > cloudwatch-config.json"
+
 
 sudo chown -R csye6225:csye6225 /opt
 sudo chmod +x /opt/csye6225/app.js
